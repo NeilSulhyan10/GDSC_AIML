2e312537f52521db9c2b59652a4e02f3a35cc09f
Neil Sulhyan <neilsulhyan@gmail.com>
2024-10-12 22:18:35 +0530

    bg done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import { useState } from 'react'
-
+import { BackgroundLines } from "./components/ui/background-lines.jsx";
+import Logo from "./assets/logo.png";
 
 function App() {
   return (
     <>
-      
+      <BackgroundLines className="flex items-center justify-center w-full flex-col px-4 bg-black">
+      <div className="absolute top-0 left-0 p-4">
+          <img src={Logo} width={125} height={125} className="animate-bounce"/>
+        </div>
+    </BackgroundLines>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
