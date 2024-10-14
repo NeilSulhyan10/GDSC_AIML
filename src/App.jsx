2ec4a0d6690c4bc4b2111922d3e9ba5d46d86079
Neil Sulhyan <neilsulhyan@gmail.com>
2024-10-14 21:54:26 +0530

    battery low 2

--- src/App.jsx
+++ src/App.jsx
@@ -15,27 +15,7 @@ function App() {
           AIML Bootcamp
         </h2>
         <Timer />
-        <div className="flex flex-col md:flex-row justify-evenly items-center w-full mt-6 font-poppins space-y-4 md:space-y-0 bg-white bg-opacity-10 backdrop-filter backdrop-blur-lg rounded-lg shadow-lg p-6">
-          <div className="md:w-1/2 px-4 md:px-0 text-center md:text-left">
-            <h3 className="text-gray-300 text-base md:text-lg lg:text-xl font-medium">
-              Google Developer Students' Club - WCE aims to inculcate the
-              culture of development and problem-solving within local
-              communities using technology.
-              <span className="block mt-4">
-                "Coming together is the beginning. Keeping together is progress.
-                Working together is success."
-              </span>
-            </h3>
-          </div>
-          <div className="md:w-1/2 mt-4 md:mt-0 flex justify-center">
-            <img
-              src={AIImage}
-              alt="AI/ML Representation"
-              className="w-1/2 h-auto"
-            />
-          </div>
-        </div>
-        <div className="flex flex-col md:flex-row justify-evenly items-center w-full mt-6 font-poppins space-y-4 md:space-y-0 bg-white bg-opacity-10 backdrop-filter backdrop-blur-lg rounded-lg shadow-lg p-6">
+        <div className="flex flex-col md:flex-row justify-evenly items-center w-full mt-6 font-poppins space-y-4 md:space-y-0 bg-white bg-opacity-10 backdrop-filter backdrop-blur-lg rounded-lg shadow-lg shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)] p-6">
           <div className="md:w-1/2 px-4 md:px-0 text-center md:text-left">
             <h3 className="text-gray-300 text-base md:text-lg lg:text-xl font-medium">
               Google Developer Students' Club - WCE aims to inculcate the
