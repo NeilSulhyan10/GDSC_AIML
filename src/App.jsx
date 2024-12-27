eda0b8567553aea813c6a8fd58971d36445d7d9e
Neil Sulhyan <neil.sulhyan@walchandsangli.ac.in>
2024-12-27 16:01:58 +0530

    removed dark

--- src/App.jsx
+++ src/App.jsx
@@ -129,7 +129,7 @@ function App() {
         </VerticalTimeline>
       </div>
       <AICard />
-      <div className="h-[40rem] rounded-md flex flex-col antialiased bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
+      <div className="h-[40rem] rounded-md flex flex-col antialiased bg-black items-center justify-center relative overflow-hidden">
         <InfiniteMovingCards
           items={shuffledImages}
           direction="right"
